bf0665f69064d7abfccfa0c986578f89f06eff11
atowi7 <dhoom.not@gmail.com>
2023-06-10 17:55:16 +0300

    third commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/*  type animation */
+
+var type = new Typed(".n-typing",
+    {
+        strings: ["my name is Abdulrahman Alatowi","I'm a Master of Computer Science Student"],
+        typeSpeed: 140,
+        BackSpeed: 50,
+        loop: true
+    }
+);
+
+var type = new Typed(".p-typing",
+    {
+        strings: ["Mobile App Developer", "Android App Developer", "IOS App Developer"],
+        typeSpeed: 100,
+        BackSpeed: 100,
+        loop: true
+    }
+);
+
+// function fun(){
+//  document.getElementById('navh').style.color=  var(--skin-color);
+// }
