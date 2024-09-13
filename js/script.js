3d1f45aa8144f3ac06a37c04692d54a016a97fe6
atowi7 <dhoom.not@gmail.com>
2024-09-13 07:49:41 +0300

    it's upgraded

--- js/script.js
+++ js/script.js
@@ -1,48 +1,91 @@
 /*Change language*/
 
-var lang = {
-    ar: {
-        title: "عربي"
-    },
-    en: {
-        title: "english"
-    }
+// var lang = {
+//     ar: {
+//         title: "عربي"
+//     },
+//     en: {
+//         title: "english"
+//     }
+// };
+
+// function changeLang(lang) {
+//     location.hash = lang;
+//     location.reload();
+// }
+
+// if (window.location.hash) {
+//     if (window.location.hash == "#ar") {
+//         document.getElementById('ar').title = lang.ar.title;
+//     } else {
+//         document.getElementById('ar').title = lang.en.title;
+//     }
+
+// }
+
+//toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
 };
 
-function changeLang(lang) {
-    location.hash = lang;
-    location.reload();
-}
+//scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
-if (window.location.hash) {
-    if (window.location.hash == "#ar") {
-        document.getElementById('ar').title = lang.ar.title;
-    } else {
-        document.getElementById('ar').title = lang.en.title;
-    }
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 250;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-}
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+
+            // active sections for animation on scroll
+            sec.classList.add('show-animate');
+        } else {
+            // repeat animation while scrolling
+            sec.classList.remove('show-animate');
+        }
+    });
+    //sticky header
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    //remove menuIcon and vav bar
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+    //add animation to footer on scroll
+    let footer = document.querySelector('footer');
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
+};
 
 /*  type animation */
 
-var type = new Typed(".n-typing",
-    {
-        strings: ["Abdulrahman Alatowi"],
-        typeSpeed: 150,
-        BackSpeed: 50,
-        loop: true
-    }
-);
+// var type = new Typed(".n-typing",
+//     {
+//         strings: ["Abdulrahman Alatowi"],
+//         typeSpeed: 150,
+//         BackSpeed: 50,
+//         loop: true
+//     }
+// );
 
-var type = new Typed(".p-typing",
-    {
-        strings: ["Mobile App Developer", "Android App Developer", "IOS App Developer"],
-        typeSpeed: 150,
-        BackSpeed: 50,
-        loop: true
-    }
-);
+// var type = new Typed(".p-typing",
+//     {
+//         strings: ["Mobile App Developer", "Android App Developer", "IOS App Developer"],
+//         typeSpeed: 150,
+//         BackSpeed: 50,
+//         loop: true
+//     }
+// );
 
-// function fun(){
-//  document.getElementById('navh').style.color=  var(--skin-color);
-// }
